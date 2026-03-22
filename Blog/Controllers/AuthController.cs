461f0f6e90223c0354357a6461cd983355b3e96d
DipeshZambre <dipeshzambre719@gmail.com>
2026-03-22 14:18:28 +0530

    Implemented the Login functionality

--- Blog/Controllers/AuthController.cs
+++ Blog/Controllers/AuthController.cs
@@ -27,6 +27,45 @@ namespace Blog.Controllers
             return View();
         }
 
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                /*
+                 ❌ Sync (Blocking)You stand at the door doing nothing until pizza arrives
+                 ✅ Async (await)You go watch TV, your phone notifies you when pizza arrives
+                 */
+
+                // await = "notify me when it's done, I'll do other things meanwhile"
+
+                var user =await _userManager.FindByEmailAsync(model.Email);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Email or Password is Incorrect");
+                    return View(model);
+                }
+
+                var signInresult = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                if (!signInresult.Succeeded)
+                {
+                    ModelState.AddModelError("", "Email or Password is Incorrect");
+                    return View(model);
+                }
+                return RedirectToAction("Index", "Post");
+            }
+            return View(model);
+        }
+
         [HttpPost]
 
         public async Task<IActionResult> Register(RegisterViewModel model)
---
+++ Blog/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is a Required Field")]
+        [EmailAddress(ErrorMessage = "Email must be in proper format")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is a Required Field")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+       
+    }
+}
